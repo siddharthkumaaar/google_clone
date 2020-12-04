146f2a49ceac6b29d657d551e1b0d37b1ca17073
siddharthkumaaar <siddharthkumaaar@gmail.com>
2020-12-04 14:32:04 +0530

    first step to adding project

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import './App.css';
+import Home from './pages/Home';
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
+
+function App() {
+  return (
+    <div className="App">
+      <Router>
+        <Switch>
+          <Route path='/search'>
+            <h1>This is the search page</h1>
+          </Route>
+          <Route>
+            <Home />
+          </Route>
+        </Switch>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
